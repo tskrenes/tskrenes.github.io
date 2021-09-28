92035a3d7b01e3e7fc50c51f16a35f84035090c6
Tyrian Skrenes <tyrian@skrenes.com>
2021-09-28 09:39:06 -0600

    Added input.

--- mario_math.js
+++ mario_math.js
@@ -1,9 +1,20 @@
+let num2, num4;
+
 function next() {
   const num = 10 + Math.random() * 90;
-  const num2 = Math.round(num);
+  num2 = Math.round(num);
   const num3 = 10 + Math.random() * 90;
-  const num4 = Math.round(num3);
+  num4 = Math.round(num3);
   $('#math').html(
     `mario had ${num2} coins then he got ${num4} more. how many coins does mario have?`
   );
+}
+
+function check() {
+  let answer = $('#answer')[0].value;
+  if (answer == num2 + num4) {
+    alert('correct!');
+  } else {
+    alert('try again.');
+  }
 }

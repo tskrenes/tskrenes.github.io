909d79eeff9fa098c823a601f507cd05cfcd0155
Tyrian Skrenes <tyrian@skrenes.com>
2021-09-29 15:47:56 -0600

    Added multiplication.

--- mario_math.js
+++ mario_math.js
@@ -18,3 +18,22 @@ function check() {
     alert('try again.');
   }
 }
+
+let num6;
+
+function next2() {
+  const num5 = 100 + Math.random() * 200;
+  num6 = Math.round(num5);
+  $('#math2').html(
+    `mario, peach, toad, luigi, wario, and donkey kong each have ${num6} coins. how many do they have in total?`
+  );
+}
+
+function check2() {
+  let answer = $('#answer2')[0].value;
+  if (answer == 6 * num6) {
+    alert('correct!');
+  } else {
+    alert('try again.');
+  }
+}
